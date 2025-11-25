7a631bf5c77900ec6e79ba6663c6ae7a6579519e
Arthur Cobbaert <arthur.cobbaert@gmail.com>
2025-11-25 18:28:35 +0100

    commit

--- get_next_line.h
+++ get_next_line.h
@@ -13,20 +13,18 @@
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
+
 # include <stdlib.h>
-# include <stdio.h>
 # include <stddef.h>
 # include <unistd.h>
-# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 3
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
 int		has_newline(char *stash);
 int		ft_strlen(char *str);
-char	*ft_strchr(char *s, int c);
 char	*ft_strdup(char *s1);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
